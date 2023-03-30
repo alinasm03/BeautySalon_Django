de5cd10cec7735457175243bd4c9fe386599e7ff
alinasmirnova0303 <alinasmirnova0303@gmail.com>
2023-03-31 00:58:01 +0300

    add HT 19 BeautySalon Django endpoints

--- services/models.py
+++ services/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    duration = models.IntegerField(default=60)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class Master(models.Model):
+    RANGE_CHOICES = (
+        (0, 'Майтер'),
+        (1, 'Топ-майстер'),
+    )
+    name = models.CharField(max_length=100)
+    range = models.IntegerField(default=0, choices=RANGE_CHOICES)
+    phone = models.IntegerField()
+    status = models.BooleanField(default=True)
+    services = models.ManyToManyField(Service)
+
+    def __str__(self):
+        return self.name
+
+
+class Booking(models.Model):
+    master = models.ForeignKey(Master, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    client = models.IntegerField()
+    date = models.DateTimeField()
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.master} {self.service} {self.client} {self.date}'
+
+
+class Schedule(models.Model):
+    master = models.ForeignKey(Master, on_delete=models.CASCADE)
+    date = models.DateField()
+    time_start = models.TimeField()
+    time_end = models.TimeField()
+
+    def __str__(self):
+        return f'{self.master} {self.date} {self.time_start} {self.time_end}'
--- services/views.py
+++ services/views.py
@@ -1,9 +1,23 @@
+import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
+from services.models import Service, Master, Schedule
 
 
 def service_handler(request):
-    return HttpResponse("Service")
+    date_start = datetime.date.today()
+    date_end = datetime.date.today()+datetime.timedelta(days=7)
+    day_of_week = date_start.weekday()
+    monday = date_start - datetime.timedelta(days=day_of_week)
+    sunday = date_start + datetime.timedelta(days=6 - day_of_week)
+    week = f'{monday} -- {sunday}'
+
+    calendars = Schedule.objects.filter(date__gte=date_start,
+                                        date__lte=date_end
+                                        ).all()
+    available_masters = Master.objects.filter(schedule__in=calendars, status=1).distinct()
+    services = Service.objects.filter(master__in=available_masters).distinct()
+    return render(request, 'service.html', {'services': list(services), 'week': week})
 
 
 def service_name_handler(request, service_name):
@@ -11,7 +25,18 @@ def service_name_handler(request, service_name):
 
 
 def specialist_handler(request):
-    return HttpResponse("Hello specialist!")
+    date_start = datetime.date.today()
+    date_end = datetime.date.today() + datetime.timedelta(days=7)
+    day_of_week = date_start.weekday()
+    monday = date_start - datetime.timedelta(days=day_of_week)
+    sunday = date_start + datetime.timedelta(days=6 - day_of_week)
+    week = f'{monday} -- {sunday}'
+
+    calendars = Schedule.objects.filter(date__gte=date_start,
+                                        date__lte=date_end
+                                        ).all()
+    available_masters = Master.objects.filter(schedule__in=calendars, status=1).distinct()
+    return render(request, 'specialist.html', {'specialists': list(available_masters), 'week': week})
 
 
 def specialist_id_handler(request, specialist_id):
