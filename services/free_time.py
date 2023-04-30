86fbc78e1eabf0a08e2dc99085b11d5df7dda39f
alinasmirnova0303 <alinasmirnova0303@gmail.com>
2023-04-30 20:36:00 +0300

    add HT 17 BeautySalon Django free windows

--- services/free_time.py
+++ services/free_time.py
@@ -31,3 +31,26 @@ def calc_free_windows(service, specialist, workday_time_start, workday_time_end)
         if no_intersection:
             free_time.append(time_start)
     return free_time
+
+
+def test_calc_free_windows(service_time, specialist, workday_time_start, workday_time_end):
+    free_time = []
+    bookings = models.Booking.objects.filter(master=specialist, date__day=workday_time_start.day).all()
+    print(bookings)
+    busy_time = []
+    for booking in bookings:
+        booking_time_start = booking.date
+        booking_time_end = booking.date + timedelta(minutes=booking['service_time'])
+        busy_time.append(convert_period_to_set(booking_time_start, booking_time_end))
+    day_times = sorted(convert_period_to_set(workday_time_start, workday_time_end - timedelta(minutes=service_time)))
+
+    for time_start in day_times:
+        possible_time = convert_period_to_set(time_start, time_start + timedelta(minutes=service_time))
+        no_intersection = True
+        for one_booking in busy_time:
+            if len(possible_time.intersection(one_booking)) > 1:
+                no_intersection = False
+                break
+        if no_intersection:
+            free_time.append(time_start)
+    return free_time
