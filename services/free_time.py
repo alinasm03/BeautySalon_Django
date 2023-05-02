6fcd93fce3d06973774cc0f0ffbbf27a1d9fb921
alinasmirnova0303 <alinasmirnova0303@gmail.com>
2023-05-02 23:39:13 +0300

    add HT 18 BeautySalon Django test free windows

--- services/free_time.py
+++ services/free_time.py
@@ -13,7 +13,6 @@ def convert_period_to_set(time_start, time_end):
 def calc_free_windows(service, specialist, workday_time_start, workday_time_end):
     free_time = []
     bookings = models.Booking.objects.filter(master=specialist, date__day=workday_time_start.day).all()
-    print(bookings)
     busy_time = []
     for booking in bookings:
         booking_time_start = booking.date
@@ -33,19 +32,17 @@ def calc_free_windows(service, specialist, workday_time_start, workday_time_end)
     return free_time
 
 
-def test_calc_free_windows(service_time, specialist, workday_time_start, workday_time_end):
+def test_calc_free_windows(bookings, service_duration, workday_time_start, workday_time_end):
     free_time = []
-    bookings = models.Booking.objects.filter(master=specialist, date__day=workday_time_start.day).all()
-    print(bookings)
     busy_time = []
     for booking in bookings:
-        booking_time_start = booking.date
-        booking_time_end = booking.date + timedelta(minutes=booking['service_time'])
+        booking_time_start = booking['date']
+        booking_time_end = booking['date'] + timedelta(minutes=booking['service_duration'])
         busy_time.append(convert_period_to_set(booking_time_start, booking_time_end))
-    day_times = sorted(convert_period_to_set(workday_time_start, workday_time_end - timedelta(minutes=service_time)))
+    day_times = sorted(convert_period_to_set(workday_time_start, workday_time_end - timedelta(minutes=service_duration)))
 
     for time_start in day_times:
-        possible_time = convert_period_to_set(time_start, time_start + timedelta(minutes=service_time))
+        possible_time = convert_period_to_set(time_start, time_start + timedelta(minutes=service_duration))
         no_intersection = True
         for one_booking in busy_time:
             if len(possible_time.intersection(one_booking)) > 1:
@@ -53,4 +50,4 @@ def test_calc_free_windows(service_time, specialist, workday_time_start, workday
                 break
         if no_intersection:
             free_time.append(time_start)
-    return free_time
+    return sorted(free_time)
--- services/tests.py
+++ services/tests.py
@@ -1,3 +1,131 @@
 from django.test import TestCase
+import datetime
+from services.free_time import convert_period_to_set, test_calc_free_windows, calc_free_windows
 
-# Create your tests here.
+
+class Test(TestCase):
+    def test_convert_period_to_set(self):
+        time_start = datetime.datetime(2022, 5, 1, 10, 0)
+        time_end = datetime.datetime(2022, 5, 1, 11, 0)
+        expected_result = {
+            datetime.datetime(2022, 5, 1, 10, 0),
+            datetime.datetime(2022, 5, 1, 10, 15),
+            datetime.datetime(2022, 5, 1, 10, 30),
+            datetime.datetime(2022, 5, 1, 10, 45),
+            datetime.datetime(2022, 5, 1, 11, 0)
+        }
+        result = convert_period_to_set(time_start, time_end)
+        self.assertSetEqual(result, expected_result)
+
+    def test_string_convert_period_to_set(self):
+        time_start = 'text'
+        time_end = datetime.datetime(2022, 5, 1, 11, 0)
+        with self.assertRaises(TypeError):
+            convert_period_to_set(time_start, time_end)
+
+    def test_reverse_convert_period_to_set(self):
+        time_end = datetime.datetime(2022, 5, 1, 10, 0)
+        time_start = datetime.datetime(2022, 5, 1, 11, 0)
+        expected_result = set()
+        result = convert_period_to_set(time_start, time_end)
+        self.assertSetEqual(result, expected_result)
+
+    def test_reverse_convert_period_to_set(self):
+        time_start = datetime.datetime(2022, 5, 1, 10, 0)
+        time_end = datetime.datetime(2022, 5, 1, 10, 0)
+        expected_result = {datetime.datetime(2022, 5, 1, 10, 0)}
+        result = convert_period_to_set(time_start, time_end)
+        self.assertSetEqual(result, expected_result)
+
+    def test_convert_period_to_set_with_timedelta(self):
+        booking = [
+            {
+            'date': datetime.datetime(2022, 5, 1, 10, 0),
+            'service_duration': 30
+            }
+        ]
+        service_duration = 30
+        work_time_start = datetime.datetime(2022, 5, 1, 10, 0)
+        work_time_end = datetime.datetime(2022, 5, 1, 11, 0)
+        result = test_calc_free_windows(booking, service_duration, work_time_start, work_time_end)
+        expected_result = [datetime.datetime(2022, 5, 1, 10, 30)]
+        self.assertListEqual(result, expected_result)
+        ####################################
+
+    def test_convert_period_to_set_with_timedelta_v2(self):
+        booking = [
+            {
+                'date': datetime.datetime(2022, 5, 1, 10, 0),
+                'service_duration': 30
+            }
+        ]
+        service_duration = 30
+        work_time_start = datetime.datetime(2022, 5, 1, 10, 0)
+        work_time_end = datetime.datetime(2022, 5, 1, 12, 0)
+        result = test_calc_free_windows(booking, service_duration, work_time_start, work_time_end)
+        expected_result = [
+                datetime.datetime(2022, 5, 1, 10, 30),
+                datetime.datetime(2022, 5, 1, 10, 45),
+                datetime.datetime(2022, 5, 1, 11, 00),
+                datetime.datetime(2022, 5, 1, 11, 15),
+                datetime.datetime(2022, 5, 1, 11, 30)
+        ]
+        self.assertListEqual(result, expected_result)
+        #############################
+    def test_convert_period_to_set_with_timedelta_v3(self):
+        booking = [
+            {
+                'date': datetime.datetime(2022, 5, 1, 10, 0),
+                'service_duration': 30
+            },
+            {
+                'date': datetime.datetime(2022, 5, 1, 10, 30),
+                'service_duration': 30
+            }
+        ]
+        service_duration = 30
+        work_time_start = datetime.datetime(2022, 5, 1, 10, 0)
+        work_time_end = datetime.datetime(2022, 5, 1, 11, 0)
+        result = test_calc_free_windows(booking, service_duration, work_time_start, work_time_end)
+
+        expected_result = []
+        self.assertListEqual(result, expected_result)
+        #############################
+    def test_convert_period_to_set_with_timedelta_v4(self):
+        booking = [
+            {
+                'date': datetime.datetime(2022, 5, 1, 10, 0),
+                'service_duration': 30
+            },
+            {
+                'date': datetime.datetime(2022, 5, 1, 10, 30),
+                'service_duration': 30
+            }
+        ]
+        service_duration = 30
+        work_time_start = datetime.datetime(2022, 5, 1, 10, 0)
+        work_time_end = datetime.datetime(2022, 5, 1, 11, 30)
+        result = test_calc_free_windows(booking, service_duration, work_time_start, work_time_end)
+
+        expected_result = [datetime.datetime(2022, 5, 1, 11, 0)]
+        self.assertListEqual(result, expected_result)
+
+        #############################
+    def test_convert_period_to_set_with_timedelta_v5(self):
+        booking = [
+            {
+                'date': datetime.datetime(2022, 5, 1, 10, 0),
+                'service_duration': 30
+            },
+            {
+                'date': datetime.datetime(2022, 5, 1, 10, 45),
+                'service_duration': 30
+            }
+        ]
+        service_duration = 30
+        work_time_start = datetime.datetime(2022, 5, 1, 10, 0)
+        work_time_end = datetime.datetime(2022, 5, 1, 11, 30)
+        result = test_calc_free_windows(booking, service_duration, work_time_start, work_time_end)
+
+        expected_result = []
+        self.assertListEqual(result, expected_result)
